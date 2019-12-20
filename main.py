ce20a3e638298cfb4bbff09ed37c10370bb1bf6f
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2019-12-20 10:07:00 +0200

    Read file correctly

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from mlxtend.data import loadlocal_mnist
+
+
+def read_files(images_path, labels_path):
+    data_set, labels = loadlocal_mnist(
+        images_path=images_path,
+        labels_path=labels_path
+    )
+
+    return data_set, labels
+
 
 if __name__ == '__main__':
-    print("Hello, world!")
+    data_set1, labels1 = read_files('test_set/t10k-images.idx3-ubyte', 'test_set/t10k-labels.idx1-ubyte')
+    data_set2, labels2 = read_files('test_set/t10k-images.idx3-ubyte', 'test_set/t10k-labels.idx1-ubyte')
+
+
+    # how to print the data in the files: (X is the data)
+    # print('Dimensions: %s x %s' % (X.shape[0], X.shape[1]))
+    # print('\n1st row', X[0])
