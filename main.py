ed305624b2c73cf87945f1d6adae9bc2e803b597
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2019-12-23 13:19:31 +0200

    Classification Error Rate

--- main.py
+++ main.py
@@ -2,15 +2,29 @@ from get_data_part_A import get_training_and_test_set
 import KnnClassifier as kNN
 import time
 
+
+def get_classification_error(classified_set, actual_classification):
+    total = 0
+    incorrect = 0
+    for index, result in enumerate(classified_set):
+        total = total + 1
+        if result != actual_classification[index]:
+            incorrect = incorrect + 1
+
+    return incorrect / total
+
+
 if __name__ == '__main__':
     training_set, training_label_set, test_data_set, test_label_set = get_training_and_test_set()
     k = 3  # 3-Nearest-Neighbor
     classifier = kNN.KnnClassifier(k, training_set, training_label_set)
 
-    samples_amount = test_data_set.shape[0]
-    start_time = time.time()
-    print(classifier.classify(test_data_set[0]))
-    elapsed_time = time.time() - start_time
-    print("Estimated time for ALL samples:")
-    print(elapsed_time*samples_amount)
+    classified_set = []
+    for sample_point in test_data_set:
+        classification = classifier.classify(sample_point)
+        classified_set.append(classification)
+
+    error_rate = get_classification_error(classified_set, test_label_set) * 100.0
+    print(error_rate)
+
 
