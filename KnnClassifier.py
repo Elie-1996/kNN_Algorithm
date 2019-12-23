ed19cb39acd69a995399b91dc1c5dfc8a96aa567
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2019-12-23 12:35:23 +0200

    Implement Classifier completely
    * Still needs testing

--- KnnClassifier.py
+++ KnnClassifier.py
@@ -1,20 +1,41 @@
-from numpy import ndarray, sum
+import numpy
 from math import sqrt
+import heapq
 
 
 class KnnClassifier:
 
-    def __init__(self, k, training_set, label_set):
-        self.k = k
+    # k should be smaller than the number of training_set samples.
+    def __init__(self, k: int, training_set, label_set):
+        self.k: int = k
         self.training_set = training_set
         self.label_set = label_set
-        self.classify()
 
     @staticmethod
     def get_euclidean_distance(sample1, sample2):
-        squared_differences: ndarray = ((sample1 - sample2)**2)
+        squared_differences: numpy.ndarray = ((sample1 - sample2)**2)  # x**2 is pow(x,2)
         return sqrt(squared_differences.sum())
 
-    # code the classification
-    def classify(self):
-        pass
+    # find the k closest samples to test_sample, and return their k indices
+    def get_k_nearest_training_samples(self, test_sample):
+
+        heap = []
+        for index, training_sample in enumerate(self.training_set):
+            distance = self.get_euclidean_distance(training_sample, test_sample)
+            heap.insert(index, (distance, index))
+        heapq.heapify(heap)
+
+        closest_k_indices = []
+        for i in range(0, self.k):
+            closest_k_indices.append(heapq.heappop(heap)[1])
+
+        return closest_k_indices
+
+    # classifies a real sample
+    def classify(self, test_sample):
+        sub_training_set_indices = self.get_k_nearest_training_samples(test_sample)
+        indices = self.label_set[sub_training_set_indices]
+        classes_counter = numpy.array([0] * 10)
+        for index in indices:
+            classes_counter[index] = classes_counter[index] + 1
+        return numpy.argmax(classes_counter)  # the classification
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from get_data_part_A import get_training_and_test_set
-
+import KnnClassifier as kNN
+import time
+import test_classification
 
 if __name__ == '__main__':
     training_set, training_label_set, test_data_set, test_label_set = get_training_and_test_set()
+    k = 3  # 3-Nearest-Neighbor
+    classifier = kNN.KnnClassifier(k, training_set, training_label_set)
+
+    samples_amount = test_data_set.shape[0]
+    start_time = time.time()
+    print(classifier.classify(test_data_set[0]))
+    elapsed_time = time.time() - start_time
+    print("Estimated time for ALL samples:")
+    print(elapsed_time*samples_amount)
+
