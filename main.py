e7abe0f56198027ef7fdc59018bb3fd7d9b3bd7a
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2020-01-02 16:50:17 +0200

    Complete kNN with report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from get_data_part_A import get_training_and_test_set
 import KnnClassifier as kNN
-from math import sqrt
 
 
 def get_classification_error(classified_set, actual_classification):
@@ -16,20 +15,31 @@ def get_classification_error(classified_set, actual_classification):
 
 if __name__ == '__main__':
     training_set, training_label_set, test_data_set, test_label_set = get_training_and_test_set()
-    k = 4  # int(sqrt(training_set.shape[0]))  # 3-Nearest-Neighbor
-    classifier = kNN.KnnClassifier(k, training_set, training_label_set)
 
+    lowest_error_and_k = (100.0, 0)
+    for k in range(1, 15):
+        classifier = kNN.KnnClassifier(k, training_set, training_label_set)
+
+        classified_set = []
+        for sample_point in training_label_set:
+            classification = classifier.classify(sample_point)
+            classified_set.append(classification)
+        error_rate = get_classification_error(classified_set, training_label_set) * 100.0
+        print('[validation] k = ' + k.__str__() + ': error = ' + error_rate.__str__())
+        if error_rate < lowest_error_and_k[0]:
+            lowest_error_and_k = (error_rate, k)
+
+    print("Lowest:")
+    print(lowest_error_and_k)
+
+    # validation on test_set
+    lowest_k = lowest_error_and_k[1]
     classified_set = []
-    counter = 0
+    classifier = kNN.KnnClassifier(lowest_k, training_set, training_label_set)
     for sample_point in test_data_set:
         classification = classifier.classify(sample_point)
         classified_set.append(classification)
-        error_rate = get_classification_error(classified_set, test_label_set) * 100.0
-        counter = counter + 1
-        print(counter.__str__() + ': (k = ' + k.__str__() + ') ' + error_rate.__str__())
-
-    print("~~~~~~~~~~~")
     error_rate = get_classification_error(classified_set, test_label_set) * 100.0
-    print(error_rate)
+    print("[test] on k = " + lowest_k.__str__() + ", error = " + error_rate.__str__())
 
 
